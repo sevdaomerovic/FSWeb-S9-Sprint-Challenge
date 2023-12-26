59225a14f48e568c942179a4a00ac6607bcf43f6
SevdaOmerovic <smedia.omerovic@gmail.com>
2023-12-26 20:17:55 +0300

    completed

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import AppFunctional from "./AppFunctional";
+import userEvent from "@testing-library/user-event";
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+beforeEach(() => {
+  render(<AppFunctional />);
+});
+
+it("Yukarıya basınca koordinat metni düzgün değişiyor mu?", async () => {
+  const upButton = screen.getByTestId("up-button");
+  userEvent.click(upButton);
+  const message = await screen.findByText(/1,2/);
+  expect(message).toBeInTheDocument();
+});
